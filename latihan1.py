035083953f6bd75dd9c511a472c2396dfe62c80b
hegarrr <115518483+hegarrr@users.noreply.github.com>
2023-01-17 21:41:43 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,24 @@
+txt  =  'Halo Dunia'
+cetak ()
+
+# Hitung jumlah karakternya
+cetak ( len ( txt ))
+
+# Ambil karakter terakhir
+cetak ( txt [ - 1 ])
+
+# Ambil indeks karakter ke-2 sampai indeks ke-4 (llo)
+cetak ( txt [ 2 : 5 ])
+
+# Hilangkan spasi pada text tersebut (HelloWorld)
+cetak ( txt .ganti ( " " , " " ))
+
+# Ubah teks menjadi huruf besar
+cetak ( txt .atas ( ) )
+
+# Ubah teks menjadi huruf kecil
+cetak ( txt .lebih rendah ( ))
+
+# Ganti karakter H dengan karakter J
+cetak ( txt . ganti ( "H" , "J" ))
+cetak ()
